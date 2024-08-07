0b97586d8bcffe241437902e2a37dbd7c986356c
Iain Majer <iainvm@outlook.com>
2024-08-08 00:36:04 +0100

    feat: Add tables for other data structures

--- infrastructure/init.sql
+++ infrastructure/init.sql
@@ -1,5 +1,31 @@
 CREATE TABLE investors (
-    id VARCHAR,
+    id VARCHAR PRIMARY KEY,
     name VARCHAR
 );
 
+CREATE TABLE deposits (
+    id VARCHAR PRIMARY KEY,
+    investor_id VARCHAR,
+    nominal INTEGER,
+    FOREIGN KEY (investor_id) REFERENCES investors(id)
+);
+
+CREATE TABLE pots (
+    id VARCHAR PRIMARY KEY,
+    deposit_id VARCHAR,
+    FOREIGN KEY (deposit_id) REFERENCES deposits(id)
+);
+
+CREATE TABLE accounts (
+    id VARCHAR PRIMARY KEY,
+    pot_id VARCHAR,
+    nominal INTEGER,
+    FOREIGN KEY (pot_id) REFERENCES pots(id)
+);
+
+CREATE TABLE receipts (
+    id VARCHAR PRIMARY KEY,
+    account_id VARCHAR,
+    amount INTEGER,
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
