2183ab45aff5b5b30614c82574af18ce25517cbc
Iain Majer <iainvm@outlook.com>
2024-08-08 14:03:42 +0100

    feat: Update proto approach

--- application/grpc/handlers/investors.go
+++ application/grpc/handlers/investors.go
@@ -25,16 +25,18 @@ func NewInvestorsHandler(service *investors.Service, log *slog.Logger) *Investor
 func (h *InvestorsHandler) Onboard(ctx context.Context, req *connect.Request[depositsv1.OnboardRequest]) (*connect.Response[depositsv1.OnboardResponse], error) {
 	h.log.With("header", req.Header()).With("request", req.Msg).Info("Onboard Called")
 
-	// Create investor
-	investor, err := h.investorsService.Create(ctx, req.Msg.Name)
+	//
+	investor, err := h.investorsService.Create(ctx, req.Msg.Investor.Name)
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInvalidArgument, err)
 	}
 
 	// Create response
 	res := connect.NewResponse(&depositsv1.OnboardResponse{
-		Id:   investor.Id.String(),
-		Name: investor.Name.String(),
+		Investor: &depositsv1.Investor{
+			Id:   investor.Id.String(),
+			Name: investor.Name.String(),
+		},
 	})
 	res.Header().Set("Investor-Version", "v1")
 	return res, nil
